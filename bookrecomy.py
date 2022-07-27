1d246396b1e2c31372caf02d3020901147e12387
Ejike Ugba <ejike.ugba@outlook.com>
2022-07-28 00:36:19 +0200

    Update bookrecomy.py

--- bookrecomy.py
+++ bookrecomy.py
@@ -58,7 +58,7 @@ DFurl = "https://raw.githubusercontent.com/ejikeugba/Statics/main/data/"
 def load_df(path):
     books = pd.read_csv(path+"BX-data.csv", sep=",",
                         on_bad_lines="skip", encoding="latin-1")  # .head(5000)
-    return books
+    return books.head(5000)
 
 
 book_df = load_df(DFurl)
